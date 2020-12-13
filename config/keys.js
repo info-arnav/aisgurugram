f4b9ce01680dca92faa570f1b26c261738119c79
info-arnav <info@arnavgupta.net>
2020-12-13 23:55:16 +0530

    database

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://Arnav:Arnav300804@cluster0.ahuqv.mongodb.net/arnavguptanetblogpost?retryWrites=true&w=majority" ||
+    "mongodb+srv://Arnav:Arnav300804@cluster0.ahuqv.mongodb.net/aisgurugram?retryWrites=true&w=majority" ||
     "mongodb://localhost/techfest",
   secretOrKey: "secret",
 };
